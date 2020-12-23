b1989e12924f5439cce053f26961e140fb556f76
Vit Horacek <mikllhor@gmail.com>
2020-12-23 12:08:05 +0100

    Styling fixed

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  
+
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
@@ -11,6 +11,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="cs">
+        <Head />
         <body>
           <Main />
           <NextScript />
